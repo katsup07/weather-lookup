2fa0726e4f48d2d3c04fa12a1113a06c840ae0e6
katsup <brett_7y7@yahoo.ca>
2023-05-24 18:06:14 +0900

    Add error messages

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,22 +1,39 @@
 const fetch = require('node-fetch');
 
 exports.getWeather = (req, res, next) => {
-  const { lat, lon } = req.body;
-  const endpoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9878f3f16a9a315010430eebb23e2ab0&units=metric`
+	const { lat, lon } = req.body;
+	const endpoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9878f3f16a9a315010430eebb23e2ab0&units=metric`;
+	const blankWeatherFields = {
+		name: '',
+		weather: [{ description: '' }],
+		main: {},
+		wind: {},
+	};
 
-  if(lon === '' || lat === '')
-    res.redirect('/');
+	if (lon === '' || lat === '')
+		return res.render(
+			'weather',
+			(weatherData = {
+				...blankWeatherFields,
+				error: 'Coordinates must be numbers and cannot be blank.',
+			})
+		);
 
-  if(lon >= -180  && lon <= 180 && lat >= -90 && lat <= 90){
-    fetch(endpoint)
-    	.then((weatherRes) => weatherRes.json())
-    	.then((weatherData) => {
-  
-        if(weatherData.name === '')
-          weatherData.name = 'Location name unknown';
-        res.render('weather', {
-          weatherData,
-        });
-      });
-  }
-}
+	if (lon >= -180 && lon <= 180 && lat >= -90 && lat <= 90) {
+		fetch(endpoint)
+			.then((weatherRes) => weatherRes.json())
+			.then((weatherData) => {
+				if (weatherData.name === '') weatherData.name = 'Location name unknown';
+				res.render('weather', {
+					weatherData,
+				});
+			});
+	} else {
+		weatherData = {
+      ...blankWeatherFields,
+			error:
+				'Coordinates must be numbers, where longitude is between -180 and 180, and latitude is between -90 and 90.',
+		};
+		res.render('weather', weatherData);
+	}
+};
